8820aaf00e93e428877d3c4c93f92e9efd41992c
Patrick McMichael <patrick@patrickmcmichael.org>
2018-09-20 16:46:22 +0100

    Add flask bcrypt and password hashing to user

--- app/extensions.py
+++ app/extensions.py
@@ -2,7 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flask_login import LoginManager
 
+from flask_bcrypt import Bcrypt
+
 
 db = SQLAlchemy()
 
 login_manager = LoginManager()
+
+bcrypt = Bcrypt()
--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,22 @@
 from datetime import datetime
 
-from app.extensions import db
+from app.extensions import db, bcrypt
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255))
-    password = db.Column(db.String(255))
+    _password = db.Column(db.String(255))
     timecreated = db.Column(db.DateTime, default=datetime.utcnow())
     verified = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = bcrypt.generate_password_hash(password, rounds=8)
+
+    def verify_pw(self, password):
+        return bcrypt.check_password_hash(self.password, password)
