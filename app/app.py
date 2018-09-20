c0e2dbc8c53b31ffdab3f8441937a38b6d7a29f7
Patrick McMichael <patrick@patrickmcmichael.org>
2018-09-20 20:27:12 +0100

    Add SteamOID and add login_manager user loader

--- app/app.py
+++ app/app.py
@@ -1,12 +1,19 @@
-from app import create_app, db
+from app import create_app, db, login_manager
 
 from app.models.user import User
 from app.models.profile import Profile
 from app.models.tracking import Tracking
+from app.models.steam_oid import SteamOID
+
 from app.steam.api import get_summaries, get_bans, get_summaries_and_bans
 
 
 app = create_app('default')
+
+
+@login_manager.user_loader
+def user_loader(id):
+    return User.query.get(int(id))
 
 
 @app.shell_context_processor
@@ -16,6 +23,7 @@ def make_shell_context():
                 User=User,
                 Profile=Profile,
                 Tracking=Tracking,
+                SteamOID=SteamOID,
                 get_summaries=get_summaries,
                 get_bans=get_bans,
                 get_summaries_and_bans=get_summaries_and_bans)
