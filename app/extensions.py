29429fc543090545c7087edc694357d216719520
Patrick McMichael <patrick@patrickmcmichael.org>
2018-09-20 17:04:07 +0100

    Add openid extension

--- app/extensions.py
+++ app/extensions.py
@@ -4,9 +4,13 @@ from flask_login import LoginManager
 
 from flask_bcrypt import Bcrypt
 
+from flask_openid import OpenID
+
 
 db = SQLAlchemy()
 
 login_manager = LoginManager()
 
 bcrypt = Bcrypt()
+
+openid = OpenID()
