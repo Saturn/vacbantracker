c8e928c7f0903f767452c80223977c0322b95437
Patrick McMichael <patrick@patrickmcmichael.org>
2018-09-20 20:27:40 +0100

    Add beginning stages of steam login views

--- app/views/auth/views.py
+++ app/views/auth/views.py
@@ -1,4 +1,11 @@
-from flask import Blueprint
+import re
+
+from flask import Blueprint, redirect, url_for
+
+from flask_login import current_user, logout_user, login_user, login_required
+
+from app.extensions import openid
+
 
 auth = Blueprint('auth', __name__)
 
@@ -11,3 +18,30 @@ def index():
 @auth.route('/register')
 def register():
     return 'This is the register route!'
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/login_with_steam')
+@openid.loginhandler
+def login_with_steam():
+    if current_user.is_authenticated:
+        return redirect(openid.get_next_url())
+    return openid.try_login('https://steamcommunity.com/openid')
+
+
+@openid.after_login
+def after_steam_login(resp):
+    """
+    If steam user is new then create new user account and attach
+    a new steam openid instance.
+    If steam user is not new then log them in
+    """
+    match = re.search('steamcommunity.com/openid/id/(.*?)$', resp.identity_url)
+    steamid = match.group(1)
+    return redirect(openid.get_next_url())
