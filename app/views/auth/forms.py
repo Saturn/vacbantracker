e062160e7f75204a94063aec25a4c84857b6a0a1
Patrick McMichael <patrick@patrickmcmichael.org>
2018-09-23 16:17:14 +0100

    Add password length validation and tidy auth forms

--- app/views/auth/forms.py
+++ app/views/auth/forms.py
@@ -1,32 +1,36 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length
+
+
+password_validation = [Length(min=8, message='Password must be at least 8 characters.'),
+                       DataRequired()]
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=password_validation)
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
 
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=password_validation)
     submit = SubmitField('Register')
 
 
 class ForgotPasswordForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 
 class NewPasswordForm(FlaskForm):
-    password = PasswordField('New Password', validators=[DataRequired()])
+    password = PasswordField('New Password', validators=password_validation)
     submit = SubmitField('Save New Password')
 
 
 class ChangePasswordForm(FlaskForm):
-    password = PasswordField('Current Password', validators=[DataRequired()])
-    new_password = PasswordField('New Password', validators=[DataRequired()])
+    password = PasswordField('Current Password', validators=password_validation)
+    new_password = PasswordField('New Password', validators=password_validation)
     submit = SubmitField('Change Password')
