98b691a13e1a35d56600e45ca8818a21f961e398
Patrick McMichael <patrick@patrickmcmichael.org>
2018-09-23 15:15:59 +0100

    Add new static css and js to base template

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-// const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 
 module.exports = {
@@ -10,21 +10,43 @@ module.exports = {
     path: path.resolve(__dirname, 'app/static/'),
     filename: 'js/app.js'
   },
+  // optimization: {
+  //   minimizer: [
+  //     new UglifyJsPlugin({
+  //       cache: true,
+  //       parallel: true,
+  //       sourceMap: true // set to true if you want JS source maps
+  //     }),
+  //     new OptimizeCSSAssetsPlugin({})
+  //   ]
+  // },
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          { loader: 'css-loader', options: { minimize: true } },
-          'sass-loader'
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
         ]
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'css/app.css'
+      filename: 'css/app.css',
+      chunkFilename: '[id].css'
     })
   ]
 };
