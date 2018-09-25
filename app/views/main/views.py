375105951e9f80aa7c3cf2ad21e36323deaebafa
Patrick McMichael <patrick@patrickmcmichael.org>
2018-09-25 18:13:41 +0100

    Add login form to homepage
    Made form snippet include card wrapping.

--- app/views/main/views.py
+++ app/views/main/views.py
@@ -2,9 +2,13 @@ from flask import Blueprint, render_template
 
 from flask_login import current_user
 
+from app.views.auth.forms import LoginForm
+
+
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
-    return render_template('index.j2')
+    form = LoginForm()
+    return render_template('index.j2', form=form)
