149f08d8e9660da290a066641ea6e546881831bd
Patrick McMichael <patrick@patrickmcmichael.org>
2018-09-29 21:47:07 +0100

    Add js flash functions

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -2,3 +2,22 @@ import 'bootstrap';
 import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
+
+
+const makeFlash = (message, category) => {
+  category = (typeof category === 'undefined') ? 'secondary' : category;
+  $(".flashes").append(`<div class="alert alert-${category} alert-dismissible fade show" role="alert">
+    ${message}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>`);
+}
+
+const clearFlashes = () => {
+  $(".flashes").html("");
+}
+
+
+window.makeFlash = makeFlash;
+window.clearFlashes = clearFlashes;
