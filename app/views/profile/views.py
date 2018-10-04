b21db58877de53660319bc4dc121c77b69b21a18
Patrick McMichael <patrick@patrickmcmichael.org>
2018-10-04 16:24:44 +0100

    Add track method

--- app/views/profile/views.py
+++ app/views/profile/views.py
@@ -1,7 +1,10 @@
 
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 
-from flask_login import login_required
+from flask_login import login_required, current_user
+
+from app.steam.id import is_steamid64
+from app.models.profile import Profile
 
 
 profile = Blueprint('profile', __name__)
@@ -15,7 +18,19 @@ def profile_view(steamid):
 @profile.route('/track', methods=('POST',))
 @login_required
 def track():
-    return "This is the track view"
+    steamid = request.form.get('steamid')
+    note = request.form.get('note')
+    if is_steamid64(steamid):
+        tracked = current_user.track_profile(Profile.get(steamid), note)
+
+    output = {'code': None, 'message': None}
+    if tracked:
+        output['code'] = 200
+        output['message'] = 'Succesfully tracked profile.'
+    else:
+        output['code'] = 400
+        output['message'] = 'Something went wrong.'
+    return jsonify(output)
 
 
 @profile.route('/untrack', methods=('POST',))
