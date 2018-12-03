154d3829a813b46f3c741b4f18b9b14f39d08889
Patrick McMichael <patrick@patrickmcmichael.org>
2018-12-03 22:16:11 +0000

    Add some tracking unit tests

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -6,6 +6,7 @@ import requests_mock
 
 
 from app.models.user import User
+from app.models.profile import Profile
 from app import db, create_app
 
 from app.steam.api import PLAYER_BANS_URL, PLAYER_SUMMARIES_URL
@@ -137,3 +138,28 @@ def test_get_steam_user(setup, steam_mock):
         steamid = '76561198066693739'
         u = User.get_or_create_steam_user(steamid)
         assert User.get_or_create_steam_user(steamid) is u
+
+
+def test_user_track_profile(setup, steam_mock):
+    u = User()
+    db.session.add(u)
+    db.session.commit()
+    with steam_mock:
+        # Get steam profile in db
+        steamid = '76561198066693739'
+        Profile.get_profiles([steamid])
+        is_tracked = u.track_profile(steamid)
+    assert is_tracked
+
+
+def test_user_already_tracking_profile(setup, steam_mock):
+    u = User()
+    db.session.add(u)
+    db.session.commit()
+    with steam_mock:
+        # Get steam profile in db
+        steamid = '76561198066693739'
+        Profile.get_profiles([steamid])
+        u.track_profile(steamid)
+        is_tracked = u.track_profile(steamid)
+    assert not is_tracked
