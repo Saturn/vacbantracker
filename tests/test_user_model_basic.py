663c44b0b896e107e2341c274508a4351065bf24
Patrick McMichael <patrick@patrickmcmichael.org>
2018-12-15 16:03:20 +0000

    Split user model tests into two files

--- tests/test_user_model_basic.py
+++ tests/test_user_model_basic.py
@@ -5,7 +5,6 @@ import pytest
 
 from app import db
 from app.models.user import User
-from app.models.profile import Profile
 
 
 def test_user_create(setup_app_and_db):
@@ -128,83 +127,3 @@ def test_change_email(setup_app_and_db):
     new_email = 'bobnew@example.com'
     u.change_email(new_email)
     assert u.email == new_email
-
-
-def test_create_steam_user(setup_app_and_db, steam_mock):
-    with steam_mock:
-        steamid = '76561198066693739'
-        u = User.get_or_create_steam_user(steamid)
-        assert u.steam_oid is not None
-        assert u.steam_oid.profile.steamid == steamid
-
-
-def test_get_steam_user(setup_app_and_db, steam_mock):
-    with steam_mock:
-        steamid = '76561198066693739'
-        u = User.get_or_create_steam_user(steamid)
-        assert User.get_or_create_steam_user(steamid) is u
-
-
-def test_is_steam_user(setup_app_and_db, steam_mock):
-    with steam_mock:
-        steamid = '76561198066693739'
-        u = User.get_or_create_steam_user(steamid)
-        assert u.steam_user
-
-
-def test_is_not_steam_user(setup_app_and_db):
-    u = User(email='bob@example.com')
-    assert not u.steam_user
-
-
-def test_user_track_profile(setup_app_and_db, steam_mock):
-    u = User()
-    db.session.add(u)
-    db.session.commit()
-    with steam_mock:
-        steamid = '76561198066693739'
-        Profile.get_profiles([steamid])
-        is_tracked = u.track_profile(steamid)
-    assert is_tracked
-
-
-def test_user_untrack_profile(setup_app_and_db, steam_mock):
-    u = User()
-    db.session.add(u)
-    db.session.commit()
-    with steam_mock:
-        steamid = '76561198066693739'
-        Profile.get_profiles([steamid])
-        u.track_profile(steamid)
-        is_untracked = u.untrack_profile(steamid)
-    assert is_untracked
-
-
-def test_user_untrack_profile_not_tracking(setup_app_and_db, steam_mock):
-    u = User()
-    db.session.add(u)
-    db.session.commit()
-    with steam_mock:
-        steamid = '76561198066693739'
-        Profile.get_profiles([steamid])
-        is_untracked = u.untrack_profile(steamid)
-    assert not is_untracked
-
-
-def test_steam_user_tracking_themselves(setup_app_and_db, steam_mock):
-    with steam_mock:
-        steamid = '76561198066693739'
-        u = User.get_or_create_steam_user(steamid)
-        assert not u.track_profile(steamid)
-
-
-def test_user_already_tracking_profile(setup_app_and_db, steam_mock):
-    u = User()
-    db.session.add(u)
-    db.session.commit()
-    with steam_mock:
-        steamid = '76561198066693739'
-        Profile.get_profiles([steamid])
-        u.track_profile(steamid)
-        is_tracked = u.track_profile(steamid)
-    assert not is_tracked
